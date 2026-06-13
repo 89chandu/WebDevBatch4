c595021aa5512309960ebcb756098a73b536440c
Chandu Bopche <82381986+89chandu@users.noreply.github.com>
2026-06-13 18:46:01 +0530

    Js day2

--- Javascript/hello.js
+++ Javascript/hello.js
@@ -1,1 +1,97 @@
-console.log("Hello Js")
+// console.log("Hello Js")
+
+// var
+// let
+// const
+
+// Redeclare
+// var name = "Rahul"
+// var name = "Raju"
+
+// console.log(name)
+
+// reassign
+// var city = "Balaghat"
+// city = "Bhopal"
+
+// console.log(city)
+
+// Function
+
+// function test() {
+
+//     var x = 10;
+//     console.log(x)
+    
+// }
+
+// if (true) {
+//     var x = 10
+// }
+// console.log(x)
+
+
+// let const
+
+// let name = "Rahul"
+// let name = "Raja" // Redeclare not allowed
+// console.log(name1)
+
+
+// let city = "Balaghat"
+// city = "Bhopal"
+
+// console.log(city)
+
+// if (true) {
+//     let x = 10
+    
+// }
+// console.log(x)
+
+// const pi = 3.14
+// const pi = 22/7
+
+// console.log(pi)
+
+// const pi = 3.14
+// pi = 22/7
+
+// console.log(pi)
+
+
+// Hosting
+// Hoisting
+
+const prompt = require("prompt-sync")();
+
+let name = prompt("Enter your name :")
+
+console.log("Hello, :",name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
