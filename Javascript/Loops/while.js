7631212910778afc0429a9ea22396e10e5188294
Chandu Bopche <82381986+89chandu@users.noreply.github.com>
2026-06-27 18:25:17 +0530

    whileloop

--- Javascript/Loops/while.js
+++ Javascript/Loops/while.js
@@ -18,5 +18,64 @@
 //     i--
 // }
 
+// even numbers
+// range 20 - 40
+
+// let i = 20
+// let count = 0
+
+// while (i <= 40) {
+
+//     console.log("Even Number :",i)
+//     count = count + 1
+    
+//     i = i + 2
+// }
+// console.log("Total Even Numbers :",count)
+
+
+// count digits
+
+// 24576
+
+// const prompt = require("prompt-sync")();
+// let num = prompt("Enter your number:")
+
+// let count = 0
+
+// let temp = num
+
+// while (num > 0) {
+//     count = count + 1
+//     num = Math.floor(num / 10)
+// }
+
+// console.log("Number of digits in " + temp + " : ", count)
+
+
+
+// reverse a number
+// 1234
+// 4321
+
+let num = 1234
+
+let reverse = 0
+
+while(num > 0) {
+    let digit  = num % 10 //last digit nikalo
+    reverse = reverse * 10 + digit // reverse ko update kro
+    num = Math.floor(num / 10) // last digit ko remove kro
+    
+}
+
+console.log(reverse)
+
+
+
+
+
+
+
 
 
