f8e57fcee988daaa4c4233452cdfee27cd2ffd88
Chandu Bopche <82381986+89chandu@users.noreply.github.com>
2026-07-09 17:57:22 +0530

    pattern

--- Javascript/Loops/pattern.js
+++ Javascript/Loops/pattern.js
@@ -4,10 +4,48 @@
 // * *
 // *
 
-for (let i = 4;i >= 1; i--) {
+// for (let i = 4;i >= 1; i--) {
 
-    for (let j = 1; j <= i ; j++) {
-        process.stdout.write(i + " ")
+//     for (let j = 1; j <= i ; j++) {
+//         process.stdout.write(i + " ")
+//     }
+//     console.log()
+// }
+
+// ****
+//  ***
+//   **
+//    *
+
+// for (i = 4; i >= 1; i--) {
+//     // print spaces
+//     for (let j = 1; j <= 4 - i; j++) {
+//         process.stdout.write(" ")
+//     }
+//     // print stars
+//     for (let k = 1; k <= i; k++) {
+//         process.stdout.write("*")
+//     }
+//     console.log()
+// }
+
+// full star pyramid
+//        *
+//      * * *
+//    * * * * *
+//  * * * * * * * 
+// * * * * * * * * *
+
+let n = 5
+for (let i = 1; i <= n; i++) {
+    // spaes
+    for(let j = 1; j <= n-i; j++) {
+         process.stdout.write(" ")
+    }
+    // stars
+    for(let k = 1; k <= (2 * i - 1); k++) {
+         process.stdout.write("*")
     }
     console.log()
 }
+
