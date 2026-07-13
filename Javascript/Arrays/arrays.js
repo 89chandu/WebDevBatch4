66c5916daec3dac035d9566500f16a2cb414b544
Chandu Bopche <82381986+89chandu@users.noreply.github.com>
2026-07-13 18:10:38 +0530

    arrays

---
+++ Javascript/Arrays/arrays.js
@@ -0,0 +1,101 @@
+// let i = 999999
+
+// arrays
+// let marks = [76,88,99,66,]
+
+// let numbers = [10,20,30,40,50,8888,222,555]
+
+// arrays the elements
+// console.log(numbers[-1])
+
+// update the values
+// numbers[2] = 100
+
+// console.log(numbers)
+
+// let arr = [10,20,30,40,50]
+
+// console.log([arr.length])
+
+
+// let data = ["Chandu",24,true,99.5,null,undefined]
+// console.log(data[3])
+
+// let arr = [
+//     [1,2],
+//     [1,4,5],
+//     [1,6],
+//     function () {
+//         console.log("Hi")
+//     }
+// ]
+// console.log(arr[3])
+
+// Basic Array Methods
+
+// push() - Last me add
+
+// let fruits = ["Apple","Mango","Kiwi"]
+// fruits.push("Banana")
+
+// console.log(fruits)
+
+// pop() - Last se remove
+
+// let fruits = ["Apple","Mango","Kiwi"]
+
+// fruits.pop()
+
+// console.log(fruits)
+
+
+// unshift() - Starting me add
+
+// let fruits = ["Apple","Mango","Kiwi"]
+
+// fruits.unshift("Samsung")
+
+// console.log(fruits)
+
+// shift() - Starting se remove
+
+// let fruits = ["Apple","Mango","Kiwi"]
+
+// fruits.shift()
+
+// console.log(fruits)
+
+// console.log(fruits.length)
+
+
+// includes()
+
+// let fruits = ["Apple","Mango","Kiwi","banana","cherry","grapes","papaya"]
+
+// console.log(fruits.includes("aam"))
+
+// indexOf()
+
+let fruits = ["Apple","Mango","Kiwi","banana","cherry","grapes","papaya"]
+
+console.log(fruits.indexOf("Kiwi"))
+
+// last
+
+// let fruits = ["Apple","Mango","Kiwi","banana","cherry","grapes","papaya"]
+
+// console.log(fruits.indexOf("Kiwi"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Javascript/Loops/pattern.js
+++ Javascript/Loops/pattern.js
@@ -125,6 +125,40 @@
 
 // Hollow Square
 
+// let n = 5
+
+// for (let i = 1; i <= n ; i++) {
+
+//     for (let j = 1; j <= n; j++) {
+
+//         if (i == 1 || i == n || j == 1 || j == n) {
+//             process.stdout.write("* ")
+
+//         } else {
+//             process.stdout.write("  ")
+//         }
+//     }
+//     console.log()
+// }
+
+// Hollow Triangle
+
+let n = 5
+
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+        if(j == 1 || j == i || i == n) {
+             process.stdout.write("* ")
+
+        } else {
+             process.stdout.write("  ")
+        }
+    }
+    console.log()
+}
+
+
+
 
 
 
